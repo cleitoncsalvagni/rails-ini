82e9f25f3fa34c7666ca918c6a46c8a2f4e605ee
Guilherme M <guimagioni@gmail.com>
2022-08-31 01:06:38 +0000

    Languages

--- PrimeiroApp_Sqlite/app/models/city.rb
+++ PrimeiroApp_Sqlite/app/models/city.rb
@@ -1,2 +1,7 @@
 class City < ApplicationRecord
+    belongs_to :state
+end
+
+class State < ApplicationRecord
+    has_many :cities
 end
--- PrimeiroApp_Sqlite/db/schema.rb
+++ PrimeiroApp_Sqlite/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_230917) do
+ActiveRecord::Schema.define(version: 2022_08_31_000104) do
 
   create_table "cities", force: :cascade do |t|
     t.string "desgription"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_08_30_230917) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "state_id"
+    t.index ["state_id"], name: "index_cities_on_state_id"
   end
 
   create_table "states", force: :cascade do |t|
