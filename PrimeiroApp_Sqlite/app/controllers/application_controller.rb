1a8067aa2a08c866f9685189c2273052d7c1f712
Guilherme M <guimagioni@gmail.com>
2022-09-13 22:42:39 +0000

    Locales

--- PrimeiroApp_Sqlite/app/controllers/application_controller.rb
+++ PrimeiroApp_Sqlite/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    def set_locale
+        if params[:locale]
+            cookies[:local] = params[:locale]
+        end
+         if cookies[:locale]
+            if I18n.locale != cookies[:locale]
+                I18n.locale = cookies[:locale]
+            end
+         end
+    end
 end
